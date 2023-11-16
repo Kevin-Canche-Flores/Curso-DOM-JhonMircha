0b2dd875fdf53a7b09a9a0b636f51725f6a98fd1
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2023-11-16 17:31:45 -0600

    optimizing btn-hamburger coding

--- JS/btn-burguer.js
+++ JS/btn-burguer.js
@@ -2,16 +2,18 @@ const $btnBurger = document.getElementById('btn-navigator');
 const $navBurger = document.getElementById('nav-navigator');
 const $navAnchors = document.querySelectorAll("nav a");
 
+const addClassToggleForNavAndBotton = () => {
+  $btnBurger.classList.toggle('is-active');
+  $navBurger.classList.toggle('display')
+}
+
 $btnBurger.addEventListener('click', (event) => {
-    $btnBurger.classList.toggle('is-active');
-    $navBurger.classList.toggle('display')
+  addClassToggleForNavAndBotton()
 });
 
 $navAnchors.forEach(navAnchor => {
   navAnchor.addEventListener('click', (event) => {
-    $btnBurger.classList.toggle('is-active');
-    $navBurger.classList.toggle('display');
-    console.log(navAnchor)
+    addClassToggleForNavAndBotton()
   })
 })
 
