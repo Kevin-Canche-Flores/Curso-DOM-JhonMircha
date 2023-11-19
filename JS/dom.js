8d3578b83d7ec5d6cefcbb1bece457be0d78613d
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2023-11-19 00:23:48 -0600

    adding boton hamburger

---
+++ JS/dom.js
@@ -0,0 +1,9 @@
+import  hamburgerMenu  from "./menu_hamburger.js";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  //Eventhandler Menu hamburguesa
+  hamburgerMenu(".hamburger", ".nav", "nav a");
+
+  //Eventhandler Clock
+  
+})
---
+++ JS/menu_hamburger.js
@@ -0,0 +1,19 @@
+export default function hamburgerMenu (btnBurger, navBurguer, menuLinks) {
+  
+  document.addEventListener("click", (event) => {
+
+    if( event.target.matches(btnBurger) || event.target.matches(`${btnBurger} *` )){  
+      document.querySelector(btnBurger).classList.toggle('is-active')
+      document.querySelector(navBurguer).classList.toggle('display')     
+    }
+
+    if(event.target.matches(menuLinks)){
+      document.querySelector(btnBurger).classList.remove('is-active')
+      document.querySelector(navBurguer).classList.remove('display')
+      console.log(event)
+    }
+  })
+  
+}
+
+
