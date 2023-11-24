3866372c88fcf59568e624927a032ba8a22f8fa5
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2023-11-24 15:29:58 -0600

    adding const time for div span = clock

--- JS/digital_clock.js
+++ JS/digital_clock.js
@@ -1,17 +1,27 @@
-export default function digitalClock (relojOn, relojOff, alarmOn, alarmOff) {
+export default function digitalClock (relojOn) {
   
   document.addEventListener("click", (event) => {
 
-    if( event.target.matches(relojOn)){  
-      document.querySelector(relojOn).classList.toggle('is-active')
-      document.querySelector(relojOn).classList.toggle('display')     
-    }
+    if(event.target.matches(relojOn) || event.target.matches(`${relojOn} *`)){
+      const time = new Date();
+      let hours = time.getHours();
+      let minutes = time.getMinutes();
+      let seconds = time.getSeconds();
+      
+      // Asegurarse de que los minutos y segundos tengan dos dígitos
+      let horasConDosDigitos = hours.toString().padStart(2, '0');
+      let minutosConDosDigitos = minutes.toString().padStart(2, '0');
+      let segundosConDosDigitos = seconds.toString().padStart(2, '0');
 
-    if(event.target.matches(menuLinks)){
-      document.querySelector(btnBurger).classList.remove('is-active')
-      document.querySelector(navBurguer).classList.remove('display')
-      console.log(event)
+
+      document.querySelector(relojOn).disabled = true;
     }
-  })
+console.log(event) 
+    // if(event.target.matches(menuLinks)){
+    //   document.querySelector(btnBurger).classList.remove('is-active')
+    //   document.querySelector(navBurguer).classList.remove('display')
+    //   console.log(event)
+    })
   
-}
+} 
+//, relojOff, alarmOn, alarmOff
--- JS/dom.js
+++ JS/dom.js
@@ -1,9 +1,12 @@
 import  hamburgerMenu  from "./menu_hamburger.js";
 import digitalClock from "./digital_clock.js";
+
 document.addEventListener("DOMContentLoaded", (event) => {
   //Eventhandler Menu hamburguesa
   hamburgerMenu(".hamburger", ".nav", "nav a");
  
   //Eventhandler Clock
-  digitalClock ('.btn-play-clock', '.btn-stop-clock', '.btn-alarm-play', '.btn-alarm-stop');
+  digitalClock(".btn-play-clock");
 })
+
+//'.btn-stop-clock', '.btn-alarm-play', '.btn-alarm-stop'
