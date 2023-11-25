49e4a772e812643cebc500e2773aaf1ac51f7610
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2023-11-24 19:33:26 -0600

    moficando la programacion del reloj

--- JS/digital_clock.js
+++ JS/digital_clock.js
@@ -5,35 +5,22 @@ export default function digitalClock(relojOn, relojOff, alarmOn, alarmOff) {
   const $alarmAudio = document.getElementById("sound-alarm");
 
   document.addEventListener("click", (event) => {
+    
     if (event.target.matches(relojOn)) {
-      //OBTENEMOS LA HORA PARA PODER PASARLO A LA VISUALIZACIÓN DEL USUARIO.
-      const updateClock = () => {
-        const time = new Date();
-        let hours = time.getHours();
-        let minutes = time.getMinutes();
-        let seconds = time.getSeconds();
 
-        // Asegurarse de que los minutos y segundos tengan dos dígitos
-        let hoursWithTwoDigits = hours.toString().padStart(2, "0");
-        let minutesWithTwoDigits = minutes.toString().padStart(2, "0");
-        let secondsWithTwoDigits = seconds.toString().padStart(2, "0");
-
-        let formatedTime = `<span>${hoursWithTwoDigits}:${minutesWithTwoDigits}:${secondsWithTwoDigits}</span>`;
-        $spanClock.innerHTML = formatedTime;
-      };
+      intervalTime = setInterval(() => {
+        let clokHour = new Date().toLocaleTimeString();
+        document.querySelector('.digital-clock').innerHTML = `<span>${clokHour}</span>`
+      }, 1000)
 
       document.querySelector(relojOn).disabled = true;
-
-      intervalTime = setInterval(updateClock, 1000);
-
-      updateClock();
     }
 
     if (event.target.matches(relojOff)) {
       //Limpiando y habilitando el botton de la hora.
       clearInterval(intervalTime);
 
-      $spanClock.innerHTML = "";
+      $spanClock.innerHTML = null;
 
       document.querySelector(relojOn).disabled = false;
     }
