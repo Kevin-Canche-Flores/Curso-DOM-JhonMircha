271f965d912c754288c343caecf5746e8ff00629
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2023-11-24 19:08:26 -0600

    adding functionality to the alarm buttons

--- JS/digital_clock.js
+++ JS/digital_clock.js
@@ -1,44 +1,53 @@
-export default function digitalClock (relojOn, relojOff, alarmOn, alarmOff) {
+export default function digitalClock(relojOn, relojOff, alarmOn, alarmOff) {
   // Declarar la variable para almacenar la referencia del intervalo
-  let intervalTime
-  let spanClock = document.querySelector('.digital-clock')
-  
-  document.addEventListener("click", (event) => {
+  let intervalTime;
+  let $spanClock = document.querySelector(".digital-clock");
+  const $alarmAudio = document.getElementById("sound-alarm");
 
-    if(event.target.matches(relojOn)){
-      
+  document.addEventListener("click", (event) => {
+    if (event.target.matches(relojOn)) {
       //OBTENEMOS LA HORA PARA PODER PASARLO A LA VISUALIZACIÓN DEL USUARIO.
       const updateClock = () => {
-      const time = new Date();
-      let hours = time.getHours();
-      let minutes = time.getMinutes();
-      let seconds = time.getSeconds();
-      
-      // Asegurarse de que los minutos y segundos tengan dos dígitos
-      let hoursWithTwoDigits = hours.toString().padStart(2, '0');
-      let minutesWithTwoDigits = minutes.toString().padStart(2, '0');
-      let secondsWithTwoDigits = seconds.toString().padStart(2, '0');
+        const time = new Date();
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
 
-      let formatedTime = `<span>${hoursWithTwoDigits}:${minutesWithTwoDigits}:${secondsWithTwoDigits}</span>`
-      spanClock.innerHTML = formatedTime; 
-    };
+        // Asegurarse de que los minutos y segundos tengan dos dígitos
+        let hoursWithTwoDigits = hours.toString().padStart(2, "0");
+        let minutesWithTwoDigits = minutes.toString().padStart(2, "0");
+        let secondsWithTwoDigits = seconds.toString().padStart(2, "0");
 
-    document.querySelector(relojOn).disabled = true;
+        let formatedTime = `<span>${hoursWithTwoDigits}:${minutesWithTwoDigits}:${secondsWithTwoDigits}</span>`;
+        $spanClock.innerHTML = formatedTime;
+      };
 
-    intervalTime = setInterval(updateClock, 1000)
+      document.querySelector(relojOn).disabled = true;
 
-    updateClock()
+      intervalTime = setInterval(updateClock, 1000);
+
+      updateClock();
     }
 
-    if(event.target.matches(relojOff)){
+    if (event.target.matches(relojOff)) {
       //Limpiando y habilitando el botton de la hora.
       clearInterval(intervalTime);
 
-      spanClock.innerHTML = '';
+      $spanClock.innerHTML = "";
 
       document.querySelector(relojOn).disabled = false;
     }
 
+    if (event.target.matches(alarmOn)) {
+      document.querySelector(alarmOn).disabled = true;
+      $alarmAudio.play();
+      $alarmAudio.loop = true;
+    }
 
-  })
+    if (event.target.matches(alarmOff)) {
+      document.querySelector(alarmOn).disabled = false;
+      $alarmAudio.pause();
+      $alarmAudio.currentTime = 0;
+    }
+  });
 }
