dbb4490163b4578c53f922a8a0c3a7cb47bd1bd9
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2024-02-23 23:11:21 -0600

    section-5: tester window

--- JS/emergy_window.js
+++ JS/emergy_window.js
@@ -1,3 +1,15 @@
-import default function emergyWindow(id, width, height) {
-  
+export default function emergyWindow(url, width, height) {
+  const $form = document.getElementById("form")
+  const windowUrl = document.getElementById(url)
+  const windowWidth = document.getElementById(width)
+  const windowHeight = document.getElementById(height)
+
+  $form.addEventListener("submit", event => {
+    event.preventDefault()
+    window.open(
+      `${windowUrl.value}`,
+      "Tester",
+      `width=${windowWidth.value},height=${windowHeight.value}`
+    ) 
+  }) 
 }
