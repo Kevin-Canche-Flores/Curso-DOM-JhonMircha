7d4a7b816ab3f49998a8efed26f7372a3d57eba6
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2024-04-25 07:01:20 -0600

    status conection

---
+++ JS/detect_connection.js
@@ -0,0 +1,22 @@
+export default function detectConnection(id) {
+  const $div = document.createElement("div")
+ 
+  const isOnLine = () => {
+    $div.classList.add("conection")
+    if(navigator.onLine){
+      $div.textContent = 'De nuevo en línea.'
+      $div.classList.add("online")
+      $div.classList.remove("offline")
+      setTimeout(()=> document.body.removeChild($div), 2000)
+    } else {
+      $div.textContent = 'Sin conexión.'
+      $div.classList.add("offline")
+      $div.classList.remove("online")
+    }
+    
+    document.body.insertBefore($div, document.body.firstChild)  
+  }
+
+  window.addEventListener("online", event => isOnLine())
+  window.addEventListener("offline", event => isOnLine())
+}
