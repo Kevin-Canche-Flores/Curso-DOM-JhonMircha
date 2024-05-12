acaaf011b8522892bf4d8be3634ee37713d94212
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2024-05-12 03:12:11 -0600

    correccion de errores de section 10

--- JS/rule.js
+++ JS/rule.js
@@ -1,9 +1,9 @@
-export default function randomRoulette(input, list) {
+export default function randomRoulette(input, players) {
  const $btn = document.getElementById(input)
- let $list = document.getElementById(list)
+ let $list = document.getElementById(players)
 
  $btn.addEventListener("click", () => {
-  let randon = Math.floor(Math.random() * ($list.childElementCount + 1))
+  let randon = Math.floor(Math.random() * ($list.childElementCount))
   alert(`El premio es para: "${$list.children[randon].textContent}"`)
  })
 }
