10ca8f246edf6feb4b9fcc969e3e75eab7984e62
Kevin Abdiel Canche Flores <114068717+Kevin-Canche-Flores@users.noreply.github.com>
2024-05-18 15:01:54 -0600

    Agregando API intersection observer al elemento slider :)

--- JS/responsive_slider.js
+++ JS/responsive_slider.js
@@ -1,13 +1,19 @@
-export default function responsiveSlider(slider, images, inputRight, inputLeft) {
+export default function responsiveSlider(boxSlider, slider, images, inputRight, inputLeft) {
+  const $sliderWrapper = document.querySelector(boxSlider);
   const $slider = document.querySelector(slider)
   const $sliderImageCount = document.querySelectorAll(images).length
   let index = 0
-
+  /*Optiones para el observador del slider */
+  let intervalSLider;
+  const options = {
+    rootMargin: "0px",
+    threshold: 0.50
+  };
+  
   function sliderNext() {
     index >= ($sliderImageCount - 1) ? index = 0 : index++
     let percentage = index * -100;
     $slider.style.transform = `translateX(${percentage}%)`;
-    
   }
 
   function sliderPrev() {
@@ -15,12 +21,27 @@ export default function responsiveSlider(slider, images, inputRight, inputLeft) 
     let percentage = index * -100;
     $slider.style.transform = `translateX(${percentage}%)`; 
   }
-
+  
   document.addEventListener("click", (e) => {
     if(e.target.matches(inputRight) || e.target.matches(`${inputRight} *`)) sliderNext()
 
     if(e.target.matches(inputLeft) || e.target.matches(`${inputLeft} *`)) sliderPrev()   
   })
 
-  setInterval(sliderNext, 7000)
+  /* APARTADO DEL INTERSECTION OBSERVER DEL ELEMENTO SLIDER */
+   intervalSLider = () => {
+    setInterval(sliderNext, 5000)
+  } 
+
+  const sliderInterval = (entries) => {
+    entries.forEach(entry => {
+     if(entry.isIntersecting) {
+      intervalSLider() 
+      console.log(entry.isIntersecting)
+    }
+    });
+  }
+
+  const observer = new IntersectionObserver(sliderInterval, options)
+  observer.observe($sliderWrapper)
 }
